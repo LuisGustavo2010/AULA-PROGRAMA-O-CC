cd55746350c5ec57f742fc5ba2d3bd8aca7b97cc
LuisGustavo2010 <49576457+LuisGustavo2010@users.noreply.github.com>
2020-03-04 22:21:39 -0300

    exercicio 2

--- main.c
+++ main.c
@@ -6,25 +6,25 @@
     //  2) Faça um programa que receba 3 numeros, calcule e mostre o produto desses numeros
     //  3) Receba 2 notas. Calcule a média ponderada (peso3 e peso7)
 
-
 int main()
 {
-   //exercicio1
-    int n1,n2,sub;
+   int n1,n2,n3;
+   int produto;
 
-    sub= n1 - n2;
+   printf("Digite o Primeiro valor: ");
+   scanf("%i",&n1);
+   printf("\n");
+   printf("Digite o Segundo valor: ");
+   scanf("%i",&n2);
+   printf("\n");
+   printf("Digite o Terceiro valor: ");
+   scanf("%i",&n3);
+   printf("\n");
 
-        printf("Digite o primeiro valor: ");
-        scanf("%i",&n1);
-        printf("\n");
-        printf("Digite o Segundo valor: ");
-        scanf("%i",&n2);
-        printf("\n");
+   produto = n1*n2*n3;
 
-        //operações é necessário inserir as perações sempre abaixo da coleta dos dados do usuário
-        sub= n1 - n2;
-        printf("O resultado da Subtracao eh: %i", sub);
+   printf("O valor do produto dos numero eh: %i",produto);
 
-    return 0;
+   return 0;
 
 }
